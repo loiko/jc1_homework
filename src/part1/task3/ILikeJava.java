ba9e54da9c568db08cdc894509ffa49e15f6f965
Nikolai Loiko <mikalai.loika@gmail.com>
2023-03-22 00:19:11 +0300

    task3 done

---
+++ src/part1/task3/ILikeJava.java
@@ -0,0 +1,8 @@
+package part1.task3;
+
+public class ILikeJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+        System.out.println("I Like Java!");
+    }
+}
