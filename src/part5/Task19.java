8a3204b3c78e4567497847af453f05ebb2cb43de
Nikolai Loiko <mikalai.loika@gmail.com>
2023-04-02 15:58:35 +0300

    task19 done

---
+++ src/part5/Task19.java
@@ -0,0 +1,9 @@
+package part5;
+
+public class Task19 {
+    public static void main(String[] args) {
+        int[] array = {3434, 232, 555, 3, 757, 3434666, 741, 77, 45243, 34534};
+        int lastElement = array[array.length - 1];
+        System.out.println(lastElement);
+    }
+}
